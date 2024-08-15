62ebfe6af45271511bab93a155031c7ca6ecea2a
kevo-tz <me@kevo.co.tz>
2024-08-15 16:26:58 +0000

    (01) intro - (06) output solution

--- exercises/01_intro/06_output/src/lib.rs
+++ exercises/01_intro/06_output/src/lib.rs
@@ -2,8 +2,21 @@ use pyo3::prelude::*;
 
 #[pyfunction]
 // TODO: Implement a function that returns a list containing the first `n` numbers in Fibonacci's sequence.
-fn fibonacci(/* n: TODO */) /* -> TODO */ {
-    todo!()
+fn fibonacci(n: i64) -> Vec<i64> {
+    match n {
+        0 => vec![],
+        1 => vec![0],
+        2 => vec![0, 1],
+        _ => {
+            let mut fib: Vec<i64> = vec![0,1];
+            while n > fib.len() as i64{
+                let fib_len = fib.len();
+                let next_val = fib[fib_len-2..].iter().sum();
+                fib.push(next_val)
+            }
+            fib
+        }
+    }
 }
 
 #[pymodule]
