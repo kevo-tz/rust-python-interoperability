fadab89e82289f9985f2ff1ddf184b7e70a589e9
kevo-tz <me@kevo.co.tz>
2024-08-17 11:08:22 +0000

    (02) classes - (00) pyclass solution

--- exercises/02_classes/00_pyclass/src/lib.rs
+++ exercises/02_classes/00_pyclass/src/lib.rs
@@ -7,8 +7,28 @@ use pyo3::prelude::*;
 //  Expose a `new_order` function to create a new `ShoppingOrder` instance.
 //  It shouldn't be possible to modify the name or the price after the object is created.
 
+#[pyclass]
+struct ShoppingOrder {
+    #[pyo3(get)]
+    name: String,
+    #[pyo3(get)]
+    price: i32,
+    #[pyo3(get, set)]
+    quantity: i32,
+}
+
+#[pyfunction]
+fn new_order(name: String, price: i32, quantity: i32) -> ShoppingOrder {
+    ShoppingOrder {
+        name,
+        price,
+        quantity,
+    }
+}
 
 #[pymodule]
 fn pyclass(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_class::<ShoppingOrder>()?;
+    m.add_function(wrap_pyfunction!(new_order, m)?)?;
     Ok(())
 }
